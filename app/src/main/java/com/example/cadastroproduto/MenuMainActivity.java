a5070d57f657236e30e9073da74629825b3da2b9
Daniel <dadani14al@gmail.com>
2019-11-02 16:58:36 -0300

    db-menu adicionado

---
+++ app/src/main/java/com/example/cadastroproduto/MenuMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cadastroproduto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_main);
+    }
+}
